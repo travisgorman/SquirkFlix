b3cad1cf51139bde3344a531df2733b3bf78e0b2
Travis Gorman <thefarbetween@me.com>
2017-04-24 15:24:25 -0500

    state lives in store at the root level

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './store'
 import Landing from './Landing'
 import Details from './Details'
 import Search from './Search'
@@ -12,16 +14,22 @@ const App = React.createClass({
   render () {
     return (
       <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match pattern='/search' component={Search} />
-          <Match pattern='/details/:id'
-            component={(props) => {
-              const shows = preload.shows.filter((show) =>
-                props.params.id === show.imdbID)
-              return <Details show={shows[0]} {...props} />
-            }} />
-        </div>
+        <Provider store={store}>
+          <div className='app'>
+
+            <Match exactly pattern='/' component={Landing} />
+
+            <Match pattern='/search' component={Search} />
+
+            <Match pattern='/details/:id'
+              component={(props) => {
+                const shows = preload.shows.filter((show) =>
+                  props.params.id === show.imdbID)
+                return <Details show={shows[0]} {...props} />
+              }} />
+
+          </div>
+        </Provider>
       </BrowserRouter>
     )
   }
